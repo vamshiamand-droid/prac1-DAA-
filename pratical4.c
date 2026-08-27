6553c073496c29e2f06c10710685827a70834ce2
vamshi <vamshiamand@gmail.com>
2026-08-27 12:06:09 +0530

    Update pratical5.c

--- pratical4.c
+++ pratical4.c
@@ -2,17 +2,51 @@
 
 int main()
 {
-    int n, i, fact = 1;
+    int n, capacity;
+    int weight[20], profit[20];
+    int dp[20][20];
 
-    printf("Enter a number: ");
+    printf("Enter number of items: ");
     scanf("%d", &n);
 
-    for(i = 1; i <= n; i++)
+    printf("Enter weights of items:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &weight[i]);
+
+    printf("Enter profits of items:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &profit[i]);
+
+    printf("Enter knapsack capacity: ");
+    scanf("%d", &capacity);
+
+    // Build DP table
+    for (int i = 0; i <= n; i++)
     {
-        fact = fact * i;
+        for (int w = 0; w <= capacity; w++)
+        {
+            if (i == 0 || w == 0)
+            {
+                dp[i][w] = 0;
+            }
+            else if (weight[i - 1] <= w)
+            {
+                int include = profit[i - 1] + dp[i - 1][w - weight[i - 1]];
+                int exclude = dp[i - 1][w];
+
+                if (include > exclude)
+                    dp[i][w] = include;
+                else
+                    dp[i][w] = exclude;
+            }
+            else
+            {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
     }
 
-    printf("Factorial = %d", fact);
+    printf("\nMaximum Profit = %d\n", dp[n][capacity]);
 
     return 0;
 }
